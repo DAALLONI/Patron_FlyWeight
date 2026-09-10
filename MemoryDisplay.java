b2edcec64e1f6c519175a28dd194c9615259818e
DAALLONI <dalozanon@udistrital.edu.co>
2026-09-10 10:21:14 -0500

    Crear MemoryDisplay.java

---
+++ MemoryDisplay.java
@@ -0,0 +1,9 @@
+public class MemoryDisplay {
+    public static long memoriaUsadaMB() {
+        Runtime runtime = Runtime.getRuntime();
+        System.gc();         
+        try { Thread.sleep(100); } catch (InterruptedException ignored) {}
+        long usada = runtime.totalMemory() - runtime.freeMemory();
+        return usada / (1024 * 1024); // convertir bytes a MB
+    }
+}
