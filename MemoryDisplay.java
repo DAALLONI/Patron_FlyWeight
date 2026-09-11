1b39efd9eedf6fe3d6ceda47ce32bf7d77a357ff
DAALLONI <dalozanon@udistrital.edu.co>
2026-09-11 15:51:29 -0500

    Delete MemoryDisplay.java
    ERROR IN THE SPACE OF MEMORY

--- MemoryDisplay.java
+++
@@ -1,9 +0,0 @@
-public class MemoryDisplay {
-    public static long memoriaUsadaMB() {
-        Runtime runtime = Runtime.getRuntime();
-        System.gc();         
-        try { Thread.sleep(100); } catch (InterruptedException ignored) {}
-        long usada = runtime.totalMemory() - runtime.freeMemory();
-        return usada / (1024 * 1024); // convertir bytes a MB
-    }
-}
